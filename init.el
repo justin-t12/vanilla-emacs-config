321db6157e81442efc8b85cc7432a796b0291c45
justin-t12 <justin.t12@outlook.com>
2024-01-21 21:57:10 -0500

    (hopefully) got config in working state with .gitignore

--- init.el
+++ init.el
@@ -1,8 +1,21 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-(package-refresh-contents)
+
+
+;; from: https://emacs.stackexchange.com/questions/50600/use-package-wont-load
+;; Bootstrap 'use-package'
+(eval-after-load 'gnutls
+  '(add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem"))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+(setq use-package-always-ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
