acce39fd8a0b44cd4cdd377d994b417aee4b8ef3
justin-t12 <justin.t12@outlook.com>
2024-06-20 08:46:45 -0400

    Started on adding completion and lsp support

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-safe-themes
+   '("36b57dcbe8262c52d3123ed30fa34e5ef6b355881674142162a8ca8e26124da9" "0af489efe6c0d33b6e9b02c6690eb66ab12998e2649ea85ab7cfedfb39dd4ac9" default))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 
@@ -16,30 +15,15 @@
 (require 'bind-key)
 (setq use-package-always-ensure t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-safe-themes
-   '("0af489efe6c0d33b6e9b02c6690eb66ab12998e2649ea85ab7cfedfb39dd4ac9" default))
- '(package-selected-packages
-   '(evil-collection undo-tree evil-org which-key modus-themes evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "./custom.el")
+
+(setq make-backup-files nil)
 
 (menu-bar-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq display-line-numbers 'relative)
+(setq global-display-line-numbers 'relative)
+(global-display-line-numbers-mode 1)
 
 ;;setting spell check program
 (setq ispell-program-name "aspell")
@@ -50,12 +34,36 @@
 ;; theme
 (use-package modus-themes
   :config
-  (load-theme 'modus-vivendi))
+  (load-theme 'modus-vivendi t))
+
+;; Display possible completions at all places
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  ;; This enables ido in all contexts where it could be useful, not just
+  ;; for selecting buffer and file names
+  (ido-mode t)
+  (ido-everywhere t)
+  ;; This allows partial matches, e.g. "uzh" will match "Ustad Zakir Hussain"
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-filename-at-point nil)
+  ;; Includes buffer names of recently opened files, even if they're not open now.
+  (setq ido-use-virtual-buffers t)
+  :diminish nil)
+
+;; Enhance M-x to allow easier execution of commands
+(use-package smex
+  :ensure t
+  :disabled t
+  :config
+  (setq smex-save-file (concat user-emacs-directory ".smex-items"))
+  (smex-initialize)
+  :bind ("M-x" . smex))
 
 ;; undo tree (mainly for evil mode)
-(use-package undo-tree
-  :config
-  (global-undo-tree-mode))
+;; (use-package undo-tree
+;;   :config
+;;   (global-undo-tree-mode))
 
 ;; evil modes
 (use-package evil
@@ -79,7 +87,6 @@
   :config
   (evil-collection-init))
 
-
 ;; which key
 (use-package which-key
   :diminish which-key-mode
@@ -88,3 +95,15 @@
   (which-key-setup-minibuffer)
   :config
   (setq which-key-idle-delay 0.3))
+
+(use-package company
+  :bind (:map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  (setq company-idle-delay 0.3)
+  (global-company-mode t))
+
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
